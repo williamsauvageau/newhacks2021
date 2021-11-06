14a9e617dad68dea3815dcab624b030db6fe05da
williamsauvageau <90654935+williamsauvageau@users.noreply.github.com>
2021-11-06 14:39:43 -0400

    Update app.py

--- app.py
+++ app.py
@@ -10,5 +10,8 @@ def index():
 def cakes():
     return 'Yummy cakes!'
 
+motivationalmessages = ["Message 1", "Message 2", "Message 3", "Message 4"]
+random.choice(motivationalmessages)
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
